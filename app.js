f7896c89a4ef34de32e7ff2cd4bdfe999b8bd777
mubashirqurashi <mubashirqurashi756@gmail.com>
2024-02-29 09:02:35 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var age = 15 ;
+alert('i am' + age + 'years old' )
+
+var brithyear =  '1-march-2001';
+document.write("My birth year is:"  + brithyear + '<br>');
+
+// // store visitors's information in variables
+var visitorName = "Ali Khan";
+var quantity = 7;
+var productTitle = " T-shirt";
+
+// Display the order confirmation message in the browser
+document.write(visitorName +  " ordered " + quantity  + productTitle + " (s) on bonanza clothing Brand.")
+ 
